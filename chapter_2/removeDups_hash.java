91f3099af4ecd5851e800e47e2f5c54938c7cb76
katlew <kat.lew.junk@gmail.com>
2015-12-13 16:35:19 -0500

    12.13 removeDuplicates_hash

---
+++ chapter_2/removeDups_hash.java
@@ -0,0 +1,25 @@
+/** 
+ * Write code to remove duplicates from an unsorted linked list
+ * Follow up 
+ * How would you solve this problem if a temporary buffer is not allowed?
+ */
+
+removeDuplicates(Node n) {
+	HashSet<Integer> set = new HashSet<Integer>();
+	Node previous = null;
+
+	while (n) {
+		if (set.contains(n.data)) 
+			previous.next = n.next;
+
+		else {
+			set.add(n.data);
+			previous = n;
+		}
+		n = n.next;
+	}
+}
+
+/** 
+ * O(N) time
+ */
