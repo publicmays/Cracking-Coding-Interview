03aafa095e6892e3fa3badd5238ce337c0cb679b
katlew <kat.lew.junk@gmail.com>
2015-12-13 15:54:17 -0500

    12.13.15 rotationString

---
+++ chapter_1/stringRotation.java
@@ -0,0 +1,16 @@
+/**
+ * 1.9 String Rotation
+ * Assume you have a method isSubstring which checks if one word is a substring of another. 
+ * Given 2 strings, s1 and s2, write code to check if s2 is a rotation of s1 using
+ * only one call to isSubstring (ex. "waterbottle" is a rotation of "erbottlewat")
+ */
+
+boolean stringRotation (String s1, String s2) {
+	int len = s1.length();
+	if (s1.length() == s2.length() && s1.length() > 0) {
+		StringBuilder rotation = new StringBuilder();
+		rotation = s1+s1;
+		return s2.isSubstring(s1);
+	}
+	return false;
+}
