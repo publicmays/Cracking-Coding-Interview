8d97ac6d2d9b5b74d1d04fa1afe17e8320bce633
katlew <kat.lew.junk@gmail.com>
2015-12-16 13:18:22 -0500

    12.16 - set of stacks

---
+++ chapter_3/stackOfPlates_followUp.java
@@ -0,0 +1,16 @@
+/**
+ * 3.3 - Stack of Plates
+ * Imagine a stack of plates, if the stack gts too high, it might topple
+ * Therefore in real life, we would like to start a new stack when the previous
+ * stack exceeds some threshold. Implement a data structure SetOfStacks. Should be composed
+ * of several stacks and sould create a new stack once the previous exceeds
+ * capacity. SetOfStacks.push() and SetOfStacks.pop() should behave identically to
+ * a single stack
+ * Follow Up
+ * Implement a function popAt(int index) which performs a pop operation on a specific
+ * sub-stack
+ */
+
+class SetOfStacks {
+	
+}
